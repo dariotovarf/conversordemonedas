0576f3945a90e8493bcb16b0ef0713734911649e
Hermen Tovar <hermendtf@gmail.com>
2024-10-16 21:12:32 -0500

    Crea la clase principal

---
+++ src/com/conversordemonedas/principal/Principal.java
@@ -0,0 +1,20 @@
+package com.conversordemonedas.principal;
+
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("""
+                ****************************************
+                MENU DE OPCIONES
+                1. Dolar (USD) a Peso Colombiano (COP)
+                2. Peso Colombiano (COP) a Dolar (USD)
+                3. Dolar (USD) a Peso Argentino (ARS)
+                5. Peso Argentino (ARS) a Dolar (USD)
+                6. Dolar (USD) a Real Brasileño (BRL)
+                7. Real Brasileño (BRL) a Dolar (USD)
+                8. Euro (EUR) a Peso Colombiano (COP)
+                9. Salir
+                Elija una opcion valida.
+                ****************************************
+                """);
+    }
+}
